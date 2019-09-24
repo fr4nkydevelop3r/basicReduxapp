d02db2198040a56b4e7a96292ae5487f32533c97
Frank Andrade <fr4nky.develop3r@gmail.com>
2019-09-24 01:00:18 -0400

    Updates

---
+++ precache-manifest.79643aa2f030db7ecbca756d17e99768.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "672839fdb97a9e2583502756e81cc115",
+    "url": "/basicReduxapp/index.html"
+  },
+  {
+    "revision": "300a45484cac2b941a9b",
+    "url": "/basicReduxapp/static/js/2.b841b6e0.chunk.js"
+  },
+  {
+    "revision": "ca704f189786fd3f3345",
+    "url": "/basicReduxapp/static/js/main.ac079edc.chunk.js"
+  },
+  {
+    "revision": "fece4f2337c8f213ccee",
+    "url": "/basicReduxapp/static/js/runtime-main.22e86029.js"
+  }
+]);
